f89557b0510789e64457f1e844bbd0edae2fa875
Stenoip Company <mystenoipcompany@gmail.com>
2025-10-19 13:47:45 -0400

    Add CORS helper for Vercel serverless functions

---
+++ api/_cors.js
@@ -0,0 +1,12 @@
+// Simple CORS helper for Vercel serverless functions
+var ALLOWED_ORIGIN = 'https://stenoip.github.io';
+
+function setCors(res) {
+  res.setHeader('Access-Control-Allow-Origin', ALLOWED_ORIGIN);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+}
+
+module.exports = {
+  setCors: setCors
+};
