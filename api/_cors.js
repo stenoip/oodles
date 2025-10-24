16a2f9c41e7f3984b8394a9fc0aacaff3c673f78
Stenoip Company <mystenoipcompany@gmail.com>
2025-10-24 19:48:40 -0400

    Update _cors.js

--- api/_cors.js
+++ api/_cors.js
@@ -1,25 +1,16 @@
-// Copyright Stenoip Company
-
-// List of allowed origins
 var ALLOWED_ORIGINS = [
   'https://stenoip.github.io',
-  'https://www.w3schools.com/html/tryit.asp?filename=tryhtml_basic' // Added the origin for the specified URL
+  'https://www.w3schools.com'
 ];
 
-function setCors(res, req) {
-  // Get the origin from the request headers
-  var origin = req.headers.origin;
+function setCors(req, res) { // Function signature needs to accept 'req' to check origin
+  const origin = req.headers.origin;
 
-  // Check if the request origin is in the allowed list
   if (ALLOWED_ORIGINS.includes(origin)) {
-    // Set the specific allowed origin header
+    // Only set the header if the request origin is in the allowed list
     res.setHeader('Access-Control-Allow-Origin', origin);
-  } else {
-    // For origins not in the allowed list, you can choose to set a default (e.g., the first one) or not set the header at all.
-
   }
 
-  // These methods and headers are typically allowed for preflight and standard requests regardless of origin
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 }
