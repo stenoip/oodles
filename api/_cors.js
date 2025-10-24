cc2ece735623b915fa6ed6112a4ade00a744a04f
Stenoip Company <mystenoipcompany@gmail.com>
2025-10-24 19:44:33 -0400

    Update _cors.js

--- api/_cors.js
+++ api/_cors.js
@@ -1,8 +1,25 @@
-// Simple CORS helper for Vercel serverless functions
-var ALLOWED_ORIGIN = 'https://stenoip.github.io';
+// Copyright Stenoip Company
 
-function setCors(res) {
-  res.setHeader('Access-Control-Allow-Origin', ALLOWED_ORIGIN);
+// List of allowed origins
+var ALLOWED_ORIGINS = [
+  'https://stenoip.github.io',
+  'https://www.w3schools.com/html/tryit.asp?filename=tryhtml_basic' // Added the origin for the specified URL
+];
+
+function setCors(res, req) {
+  // Get the origin from the request headers
+  var origin = req.headers.origin;
+
+  // Check if the request origin is in the allowed list
+  if (ALLOWED_ORIGINS.includes(origin)) {
+    // Set the specific allowed origin header
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  } else {
+    // For origins not in the allowed list, you can choose to set a default (e.g., the first one) or not set the header at all.
+
+  }
+
+  // These methods and headers are typically allowed for preflight and standard requests regardless of origin
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 }
