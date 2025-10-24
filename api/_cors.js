2cdd0f7795168e3fc10fc8d72d23ec0facabf389
Stenoip Company <mystenoipcompany@gmail.com>
2025-10-24 19:50:06 -0400

    Update _cors.js

--- api/_cors.js
+++ api/_cors.js
@@ -1,20 +1,11 @@
-var ALLOWED_ORIGINS = [
-  'https://stenoip.github.io',
-  'https://www.w3schools.com'
-];
+var ALLOWED_ORIGIN = 'https://stenoip.github.io';
 
-function setCors(req, res) { // Function signature needs to accept 'req' to check origin
-  const origin = req.headers.origin;
-
-  if (ALLOWED_ORIGINS.includes(origin)) {
-    // Only set the header if the request origin is in the allowed list
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-
+function setCors(res) {
+  res.setHeader('Access-Control-Allow-Origin', ALLOWED_ORIGIN);
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 }
 
 module.exports = {
   setCors: setCors
-};
+}; 
