a7c6e93a071838c973e7de94514aac2b3e725ad1
Stenoip Company <mystenoipcompany@gmail.com>
2026-03-31 12:28:12 -0400

    Fix multiple origins error

--- api/_cors.js
+++ api/_cors.js
@@ -1,7 +1,17 @@
-var ALLOWED_ORIGIN = 'https://stenoip.github.io, http://localhost:9000';
+var ALLOWED_ORIGINS = [
+  'https://stenoip.github.io',
+  'http://localhost:9000'
+];
 
-function setCors(res) {
-  res.setHeader('Access-Control-Allow-Origin', ALLOWED_ORIGIN);
+function setCors(req, res) {
+  var origin = req.headers.origin;
+
+  // Check if the incoming origin is in our allowed list
+  if (ALLOWED_ORIGINS.includes(origin)) {
+    // res.set is common in Express; use res.setHeader for native Node.js
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 }
