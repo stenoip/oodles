71761220affc4cb58c859d7d283355ba5cc1ed61
Stenoip Company <mystenoipcompany@gmail.com>
2026-05-14 13:23:04 -0400

    Update _cors.js to have localhost

--- api/_cors.js
+++ api/_cors.js
@@ -1,7 +1,16 @@
-var ALLOWED_ORIGIN = 'https://stenoip.github.io';
+var ALLOWED_ORIGINS = [
+  'https://stenoip.github.io',
+  'http://localhost:1010'
+];
 
-function setCors(res) {
-  res.setHeader('Access-Control-Allow-Origin', ALLOWED_ORIGIN);
+function setCors(req, res) {
+  var origin = req.headers.origin;
+
+  // Check if the incoming origin is in our allowed list
+  if (ALLOWED_ORIGINS.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 }
